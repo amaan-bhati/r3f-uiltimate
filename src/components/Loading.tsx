7e9e15fbd900db3d30d63f3a25a13755ecc82615
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2025-01-12 20:57:22 +0530

    performance: implement Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,15 @@
+// import { p } from "framer-motion/client";
+import { SVGProps } from "react";
+
+export function HorizontalLine(props: SVGProps<SVGSVGElement>) {
+  return (
+   <p>logo</p>
+  );
+}
+
+export function VerticalLine(props: SVGProps<SVGSVGElement>) {
+  return (
+    <p>logo</p>
+    
+  );
+}
