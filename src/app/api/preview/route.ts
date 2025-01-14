f381c514ea5bfbeb926b5fb538224e8b6c27e510
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2025-01-14 22:41:45 +0530

    refactor: comment out unused imports and API function in preview route for future integration

--- src/app/api/preview/route.ts
+++ src/app/api/preview/route.ts
@@ -1,10 +1,13 @@
-import { NextRequest } from "next/server";
-import { redirectToPreviewURL } from "@prismicio/next";
+// import { NextRequest } from "next/server";
+// import { redirectToPreviewURL } from "@prismicio/next";
 
-import { createClient } from "../../../prismicio";
+// import { createClient } from "../../../prismicio";
 
-export async function GET(request: NextRequest) {
-  const client = createClient();
+// export async function GET(request: NextRequest) {
+//   const client = createClient();
 
-  return await redirectToPreviewURL({ client, request });
-}
+//   return await redirectToPreviewURL({ client, request });
+// }
+
+
+//codtaken from the doc, needs to be updated after integration
